655c3061e438b3747d9b2fb3261629721601d7a6
huangjiaxi <trustfor@sina.cn>
2020-03-19 22:28:14 +0800

    week10 2-1 SpringBoot最基础的编程模式@Component+@Autowired

--- src/main/java/com/lin/missyou/api/v1/BannerController.java
+++ src/main/java/com/lin/missyou/api/v1/BannerController.java
@@ -1,29 +1,22 @@
 package com.lin.missyou.api.v1;
 
-import org.springframework.stereotype.Controller;
+import com.lin.missyou.sample.hero.Diana;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@Controller
+@RestController
+@RequestMapping("/v1/banner")
 public class BannerController {
-    // MVC SpringMVC
-    // Controller model view
 
-    @GetMapping("/test0")
-    public void test(HttpServletResponse response) throws IOException {
-        // http://localhost:8080/test0
-        // 看到的是乱码  Hello,??
-        response.getWriter().write("Hello,亚瑟");
-    }
+    @Autowired
+    private Diana diana;
 
     @GetMapping("/test")
-    @ResponseBody
     public String test2() {
-        // 如果是文本 header text/plain
-        // 如果是对象 header 会标记 application/json
+        diana.q();
         return "Hello,亚瑟";
     }
 }
