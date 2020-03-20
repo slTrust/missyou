a4f2554605e92bbfe56b98b001ffc4260741d003
huangjiaxi <trustfor@sina.cn>
2020-03-21 00:37:37 +0800

    week11  1-7 @ConditionalOnBean条件注解

--- src/main/java/com/lin/missyou/sample/HeroConfiguration.java
+++ src/main/java/com/lin/missyou/sample/HeroConfiguration.java
@@ -5,6 +5,8 @@ import com.lin.missyou.sample.condition.IreliaCondition;
 import com.lin.missyou.sample.hero.Camille;
 import com.lin.missyou.sample.hero.Diana;
 import com.lin.missyou.sample.hero.Irelia;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -13,7 +15,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HeroConfiguration {
 
+    @Bean
+//    @ConditionalOnBean(name="mysql") // ioc 里有 mysql这个 bean ,就把它 放入容器
+    @ConditionalOnMissingBean(name="mysql") // ioc 里 不存在 mysql这个 bean 条件成立
+    public ISkill diana(){
+        return new Diana();
+    }
 
+
+    /*
     @Bean
     @ConditionalOnProperty(value="hero.condition",havingValue = "diana")
     public ISkill diana(){
@@ -25,7 +35,6 @@ public class HeroConfiguration {
     public ISkill irelia(){
         return new Irelia();
     }
-    /*
     @Bean
     @Conditional(DianaCondition.class)
     public ISkill diana(){
