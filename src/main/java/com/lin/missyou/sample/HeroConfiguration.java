67480ebb5d09e42cca11d6ba440620acfb83ade1
huangjiaxi <trustfor@sina.cn>
2020-03-21 00:24:37 +0800

    week11 1-6 成品条件组件@ConditionalOnProperty

--- src/main/java/com/lin/missyou/sample/HeroConfiguration.java
+++ src/main/java/com/lin/missyou/sample/HeroConfiguration.java
@@ -5,6 +5,7 @@ import com.lin.missyou.sample.condition.IreliaCondition;
 import com.lin.missyou.sample.hero.Camille;
 import com.lin.missyou.sample.hero.Diana;
 import com.lin.missyou.sample.hero.Irelia;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HeroConfiguration {
 
+
+    @Bean
+    @ConditionalOnProperty(value="hero.condition",havingValue = "diana")
+    public ISkill diana(){
+        return new Diana();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value="hero.condition",havingValue = "irelia")
+    public ISkill irelia(){
+        return new Irelia();
+    }
+    /*
     @Bean
     @Conditional(DianaCondition.class)
     public ISkill diana(){
@@ -23,4 +37,5 @@ public class HeroConfiguration {
     public ISkill irelia(){
         return new Irelia();
     }
+    */
 }
