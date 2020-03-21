fb4e91b292b6dbb47e03b943b24d893d2e1c8d98
huangjiaxi <trustfor@sina.cn>
2020-03-21 18:16:22 +0800

    week11 2-5 @Import 的两种用法

--- src/main/java/com/lin/missyou/LOLApplication.java
+++ src/main/java/com/lin/missyou/LOLApplication.java
@@ -1,14 +1,22 @@
 package com.lin.missyou;
 
+import com.lin.missyou.sample.HeroConfiguration;
 import com.lin.missyou.sample.ISkill;
+import com.lin.missyou.sample.hero.LOLConfigurationSelector;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@ComponentScan
+//@ComponentScan
+//@Import(HeroConfiguration.class)
+@Import(LOLConfigurationSelector.class)
 public class LOLApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = new SpringApplicationBuilder(LOLApplication.class).run(args);
+        ConfigurableApplicationContext context = new SpringApplicationBuilder(LOLApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
 
         ISkill iSkill = (ISkill) context.getBean("irelia");
         iSkill.r();
---
+++ src/main/java/com/lin/missyou/sample/hero/LOLConfigurationSelector.java
@@ -0,0 +1,12 @@
+package com.lin.missyou.sample.hero;
+
+import com.lin.missyou.sample.HeroConfiguration;
+import org.springframework.context.annotation.ImportSelector;
+import org.springframework.core.type.AnnotationMetadata;
+
+public class LOLConfigurationSelector implements ImportSelector {
+    @Override
+    public String[] selectImports(AnnotationMetadata annotationMetadata) {
+        return new String[]{HeroConfiguration.class.getName()};
+    }
+}
