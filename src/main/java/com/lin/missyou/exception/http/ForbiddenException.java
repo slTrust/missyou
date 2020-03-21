60aab4089d1bb29a71b9b953ad765d2850b09c2a
huangjiaxi <trustfor@sina.cn>
2020-03-21 23:01:23 +0800

    week12 1-5 自定义HTTPException异常类

---
+++ src/main/java/com/lin/missyou/exception/http/ForbiddenException.java
@@ -0,0 +1,8 @@
+package com.lin.missyou.exception.http;
+
+public class ForbiddenException extends HttpException {
+    public ForbiddenException(int code){
+        this.code = code;
+        this.httpStatusCode = 403;
+    }
+}
---
+++ src/main/java/com/lin/missyou/exception/http/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.lin.missyou.exception.http;
+
+public class NotFoundException extends HttpException {
+    public NotFoundException(int code){
+        this.httpStatusCode = 404;
+        this.code = code;
+    }
+}
