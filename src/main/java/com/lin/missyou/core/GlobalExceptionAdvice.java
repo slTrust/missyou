5ad7f4cb2f67922a4d4b5129e20f32242ed55ed3
huangjiaxi <trustfor@sina.cn>
2020-03-21 23:40:11 +0800

    week12  2-2 处理自定义HTTPException

--- src/main/java/com/lin/missyou/core/GlobalExceptionAdvice.java
+++ src/main/java/com/lin/missyou/core/GlobalExceptionAdvice.java
@@ -1,7 +1,10 @@
 package com.lin.missyou.core;
 
 import com.lin.missyou.exception.http.HttpException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,8 +28,18 @@ public class GlobalExceptionAdvice {
     }
 
     @ExceptionHandler(value=HttpException.class)
-    public UnifyResponse handlerHttpException(HttpServletRequest req, HttpException e) {
-        UnifyResponse message = new UnifyResponse(9999,"服务器异常2","url");
-        return message;
+    public ResponseEntity<UnifyResponse> handlerHttpException(HttpServletRequest req, HttpException e) {
+        String requestUrl = req.getRequestURI();
+        String method = req.getMethod();
+
+        UnifyResponse message = new UnifyResponse(e.getCode(),"xxx",method + " " + requestUrl);
+
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpStatus httpStatus = HttpStatus.resolve(e.getHttpStatusCode());
+
+        ResponseEntity<UnifyResponse> r = new ResponseEntity<>(message,header,httpStatus);
+        return r;
     }
 }
--- src/main/java/com/lin/missyou/exception/http/HttpException.java
+++ src/main/java/com/lin/missyou/exception/http/HttpException.java
@@ -3,4 +3,12 @@ package com.lin.missyou.exception.http;
 public class HttpException extends RuntimeException {
     protected Integer code;
     protected Integer httpStatusCode = 500;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public Integer getHttpStatusCode() {
+        return httpStatusCode;
+    }
 }
