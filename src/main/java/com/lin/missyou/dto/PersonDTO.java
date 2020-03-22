6f9bedddad9369d0242f53a4565b804dbf8217f0
huangjiaxi <trustfor@sina.cn>
2020-03-22 22:07:30 +0800

    week13 2-5 自定义校验注解的关联类(1)

--- src/main/java/com/lin/missyou/dto/PersonDTO.java
+++ src/main/java/com/lin/missyou/dto/PersonDTO.java
@@ -1,5 +1,6 @@
 package com.lin.missyou.dto;
 
+import com.lin.missyou.validators.PasswordEqual;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +12,14 @@ import javax.validation.Valid;
 
 @Builder
 @Getter
+@PasswordEqual
 public class PersonDTO {
     @Length(min=2,max=10,message = "xxx")
     private String name;
     private Integer age;
 
-    @Valid
-    private SchoolDTO schoolDTO;
+
+    private String password1;
+    private String password2;
 }
+
---
+++ src/main/java/com/lin/missyou/validators/PasswordValidator.java
@@ -0,0 +1,28 @@
+package com.lin.missyou.validators;
+
+import com.lin.missyou.dto.PersonDTO;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordValidator implements ConstraintValidator<PasswordEqual, PersonDTO> {
+
+    /*
+    ConstraintValidator<> 是一个泛型接口
+    接受两个参数
+        第一个是 自定义注解类型 PasswordEqual
+        第二个是 这个自定义注解 PasswordEqual 修饰的 目标类型，你要校验哪个DTO 你就加在谁身上（PersonDTO）
+    实现 isValid 方法 具体的校验逻辑
+    */
+
+    @Override
+    public boolean isValid(PersonDTO personDTO, ConstraintValidatorContext constraintValidatorContext) {
+        String password1 = personDTO.getPassword1();
+        String password2 = personDTO.getPassword2();
+        boolean match = password1.equals(password2);
+        return match;
+    }
+
+
+
+}
