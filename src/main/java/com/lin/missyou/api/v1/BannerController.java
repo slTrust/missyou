9ce0fb73daa322a3e65337e7272b57a57c4babc3
huangjiaxi <trustfor@sina.cn>
2020-03-22 22:57:50 +0800

    week14 1-2 Service业务逻辑层(服务层）的建立

--- src/main/java/com/lin/missyou/api/v1/BannerController.java
+++ src/main/java/com/lin/missyou/api/v1/BannerController.java
@@ -1,14 +1,27 @@
 package com.lin.missyou.api.v1;
 
 import com.lin.missyou.dto.PersonDTO;
+import com.lin.missyou.service.BannerService;
 import org.hibernate.validator.constraints.Range;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping("/banner")
 @Validated
 public class BannerController {
+
+    @Autowired
+    private BannerService bannerService;
+
+    @GetMapping("/name/{name}")
+    public void getByName(@PathVariable @NotBlank String name){
+
+
+    }
 
     @PostMapping("/test/{id}")
     public PersonDTO test5(@PathVariable @Range(min = 1,max= 10,message = "不能大于10小于1") Integer id,
