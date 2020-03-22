cdc3d0a4f818a90e98b10515956daf2407ec2ca2
huangjiaxi <trustfor@sina.cn>
2020-03-22 18:59:21 +0800

    week13  1-5 LomBok的基本使用方式

--- src/main/java/com/lin/missyou/dto/PersonDTO.java
+++ src/main/java/com/lin/missyou/dto/PersonDTO.java
@@ -1,22 +1,14 @@
 package com.lin.missyou.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+//@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class PersonDTO {
+    @NonNull
     private String name;
     private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
