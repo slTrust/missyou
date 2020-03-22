124ad448b67ae4d8645d4b81e51e7a0dea0684e6
huangjiaxi <trustfor@sina.cn>
2020-03-22 18:41:16 +0800

    week13 1-4 post请求参数接收的正确姿势

--- src/main/java/com/lin/missyou/api/v1/BannerController.java
+++ src/main/java/com/lin/missyou/api/v1/BannerController.java
@@ -1,7 +1,10 @@
 package com.lin.missyou.api.v1;
 
+import com.lin.missyou.dto.PersonDTO;
 import com.lin.missyou.exception.http.NotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -20,6 +23,24 @@ public class BannerController {
 
     @GetMapping("/test3/{id}")
     public String test3(@PathVariable Integer id, @RequestParam String name){
+        return "id:" + id + ",name:" + name;
+    }
+
+    // 获取请求体内容
+    @PostMapping("/test4/{id}")
+    public String test4(@PathVariable Integer id,
+                        @RequestParam String name,
+                        @RequestBody Map<String,Object> person){
+        // 一般不会用 因为是 Map 会有一个转型问题 处理非常麻烦
+        // 在 java里 最好对应一个类 去映射 对应的 字段
+        return "id:" + id + ",name:" + name;
+    }
+
+    @PostMapping("/test5/{id}")
+    public String test5(@PathVariable Integer id,
+                        @RequestParam String name,
+                        @RequestBody PersonDTO person){
+        System.out.println(person.getName() + "," + person.getAge());
         return "id:" + id + ",name:" + name;
     }
 
