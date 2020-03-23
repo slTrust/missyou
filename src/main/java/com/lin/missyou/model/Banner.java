a2852721af009e69a1b31aa030a1a992d1e30584
huangjiaxi <trustfor@sina.cn>
2020-03-23 10:48:30 +0800

    week14 2-4 常用实体类注解与ORM生成表的优劣势浅谈

--- src/main/java/com/lin/missyou/model/Banner.java
+++ src/main/java/com/lin/missyou/model/Banner.java
@@ -1,13 +1,17 @@
 package com.lin.missyou.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name="banner1")
 public class Banner {
     @Id
     private long id;
+
+    @Column(length = 16)
     private String name;
+
+    @Transient // 这样 数据库就不会生成 这个字段
     private String description;
     private String img;
     private String title;
