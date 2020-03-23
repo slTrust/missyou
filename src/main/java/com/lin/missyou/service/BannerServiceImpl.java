2029635856dde4331df736ed35d2fbc867563a29
huangjiaxi <trustfor@sina.cn>
2020-03-23 17:15:48 +0800

    week14 3-3 JPA查询的懒加载和急加载

--- src/main/java/com/lin/missyou/service/BannerServiceImpl.java
+++ src/main/java/com/lin/missyou/service/BannerServiceImpl.java
@@ -5,7 +5,7 @@ import com.lin.missyou.repository.BannerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//@Service
+@Service
 public class BannerServiceImpl implements BannerService {
     @Autowired
     private BannerRepository bannerRepository;
