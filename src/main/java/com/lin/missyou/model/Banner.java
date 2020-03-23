8d6ac716934e380bd663f8fde26ea7f996cc3932
huangjiaxi <trustfor@sina.cn>
2020-03-23 10:36:55 +0800

    week14 2-3 使用ORM的方式生成数据表

--- src/main/java/com/lin/missyou/model/Banner.java
+++ src/main/java/com/lin/missyou/model/Banner.java
@@ -1,10 +1,11 @@
 package com.lin.missyou.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Banner {
-
+    @Id
     private long id;
     private String name;
     private String description;
