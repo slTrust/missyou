e088d91060acc5775b7f073efdf179a359c04b77
slTrust <trustfor@sina.cn>
2020-03-25 16:13:28 +0800

    week15 3-2 Spu实体映射

---
+++ src/main/java/com/lin/missyou/model/SpuEntity.java
@@ -0,0 +1,32 @@
+package com.lin.missyou.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+public class SpuEntity extends BaseEntity{
+    @Id
+    private Long id;
+    private String title;
+    private String subtitle;
+    private Long categoryId;
+    private Long rootCategoryId;
+    private byte online;
+    private String price;
+    private Long sketchSpecId;
+    private Long defaultSkuId;
+    private String img;
+    private String discountPrice;
+    private String description;
+    private String tags;
+    private byte isTest;
+
+//    private Object spuThemeImg;
+    private String forThemeImg;
+}
