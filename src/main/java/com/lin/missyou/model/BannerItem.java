818f394914497c52a29b774a7208e2d268c83b96
huangjiaxi <huangjiaxi@oak-inc.com>
2020-03-25 10:57:15 +0800

    week15 1-8 提取 BaseEntity 基类

--- src/main/java/com/lin/missyou/model/BannerItem.java
+++ src/main/java/com/lin/missyou/model/BannerItem.java
@@ -4,22 +4,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
-public class BannerItem {
+public class BannerItem extends BaseEntity{
     @Id
     private Long id;
     private String img;
     private String keyword;
     private Short type;
-    private Date createTime;
-    private Date updateTime;
-    private Date deleteTime;
     private Long bannerId;
     private String name;
 }
