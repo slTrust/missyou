91253534c8aa64f462f0c4262daf5a86caf98294
slTrust <trustfor@sina.cn>
2020-03-25 13:14:43 +0800

    week15 2-3 Jaskson序列化库的重要配置（SNAKE_CASE、时间戳)

--- src/main/java/com/lin/missyou/model/BaseEntity.java
+++ src/main/java/com/lin/missyou/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.lin.missyou.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,10 @@ import java.util.Date;
 @MappedSuperclass
 public abstract class BaseEntity {
 
+    @JsonIgnore
     private Date createTime;
+    @JsonIgnore
     private Date updateTime;
+    @JsonIgnore
     private Date deleteTime;
 }
