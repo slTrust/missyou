b812cba4cdea1e1a5a8c1b533d6f0ad217ff2083
slTrust <trustfor@sina.cn>
2020-03-25 21:55:09 +0800

    week15 3-9 VO视图层对象的概念和意义

---
+++ src/main/java/com/lin/missyou/vo/SpuSimplifyVO.java
@@ -0,0 +1,16 @@
+package com.lin.missyou.vo;
+
+public class SpuSimplifyVO {
+    private Long id;
+    private String title;
+    private String subtitle;
+
+    private String img;
+    private String forThemeImg;
+    private String price;
+    private String discountPrice;
+
+    private String description;
+    private String tags;
+    private Long sketchSpecId;
+}
