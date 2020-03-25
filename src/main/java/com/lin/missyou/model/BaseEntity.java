a5c9881fa4920c78b07e2cf2ad24bbce305a084f
slTrust <trustfor@sina.cn>
2020-03-25 12:00:50 +0800

    week15 2-2 @MappedSuperclass 的作用

--- src/main/java/com/lin/missyou/model/BaseEntity.java
+++ src/main/java/com/lin/missyou/model/BaseEntity.java
@@ -3,10 +3,12 @@ package com.lin.missyou.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseEntity {
 
     private Date createTime;
