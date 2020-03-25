258ad345e5878ecb9f7cdcc45c318b47299987ea
slTrust <trustfor@sina.cn>
2020-03-25 23:14:49 +0800

    week16 1-4 Paging分页对象的构建

--- src/main/java/com/lin/missyou/api/v1/SpuController.java
+++ src/main/java/com/lin/missyou/api/v1/SpuController.java
@@ -2,12 +2,15 @@ package com.lin.missyou.api.v1;
 
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
+import com.lin.missyou.bo.PageCounter;
 import com.lin.missyou.exception.http.NotFoundException;
 import com.lin.missyou.model.Spu;
 import com.lin.missyou.service.SpuService;
+import com.lin.missyou.util.CommonUtil;
 import com.lin.missyou.vo.SpuSimplifyVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,8 +45,10 @@ public class SpuController {
     @GetMapping("/latest")
     public List<SpuSimplifyVO> getLatestSpuList(@RequestParam(defaultValue = "0") Integer start,
                                                 @RequestParam(defaultValue = "10") Integer size){
+        PageCounter pageCounter = CommonUtil.convertToPageParamter(start,size);
+        Page<Spu> spuList = this.spuService.getLatestPagingSpu(pageCounter.getPage(),pageCounter.getCount());
+
         Mapper mapper = DozerBeanMapperBuilder.buildDefault();
-        List<Spu> spuList = this.spuService.getLatestPagingSpu();
         List<SpuSimplifyVO> vos = new ArrayList<>();
         spuList.forEach(s->{
             // s 代表 Spu对象，
