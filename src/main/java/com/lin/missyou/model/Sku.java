b37434f54f3a54129b7d0cbead7497a53dfeff9c
slTrust <trustfor@sina.cn>
2020-03-27 16:01:08 +0800

    week17 1-3 通用泛型类与List的转换(2), TypeReference 使用

--- src/main/java/com/lin/missyou/model/Sku.java
+++ src/main/java/com/lin/missyou/model/Sku.java
@@ -1,13 +1,18 @@
 package com.lin.missyou.model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.lin.missyou.util.GenericAndJson;
 import com.lin.missyou.util.ListAndJson;
+import com.sun.tools.javah.Gen;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.lang.reflect.Type;
 import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
@@ -26,10 +31,28 @@ public class Sku extends BaseEntity{
     private Long categoryId;
     private Long rootCategoryId;
 
-    @Convert(converter = ListAndJson.class)
-    private List<Object> specs;
+//    @Convert(converter = ListAndJson.class)
+//    private List<Object> specs;
+
+    private String specs;
 
     private String code;
     private Long stock;
 
+    public List<Spec> getSpecs() {
+        if(this.specs == null){
+            return Collections.emptyList();
+        }
+        // 报错 这里是不能 List<Spec>的
+        // return GenericAndJson.jsonToList(this.specs,List<Spec>.class);
+        // 而是用 TypeReference<T>
+        return GenericAndJson.jsonToList(this.specs, new TypeReference<List<Spec>>() {});
+    }
+
+    public void setSpecs(List<Spec> specs) {
+        if(specs.isEmpty()){
+            return;
+        }
+        this.specs = GenericAndJson.objectToJson(specs);
+    }
 }
--- src/main/java/com/lin/missyou/util/GenericAndJson.java
+++ src/main/java/com/lin/missyou/util/GenericAndJson.java
@@ -1,10 +1,13 @@
 package com.lin.missyou.util;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lin.missyou.exception.http.ServerErrorException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GenericAndJson {
 
     private static ObjectMapper mapper;
@@ -36,5 +39,19 @@ public class GenericAndJson {
            throw new ServerErrorException(9999);
        }
    }
+
+   public static <T> T jsonToList(String s, TypeReference<T> tr){
+       try {
+           if(s==null){
+               return null;
+           }
+           T list = GenericAndJson.mapper.readValue(s, tr);
+           return list;
+       } catch (JsonProcessingException e) {
+           e.printStackTrace();
+           throw new ServerErrorException(9999);
+       }
+   }
+
 
 }
