ec16ee17283c09a815197758dcf3cdd138aaabdc
slTrust <trustfor@sina.cn>
2020-03-30 20:48:54 +0800

    week17 5-1 获取用户OpenId

--- src/main/java/com/lin/missyou/api/v1/TokenController.java
+++ src/main/java/com/lin/missyou/api/v1/TokenController.java
@@ -2,6 +2,8 @@ package com.lin.missyou.api.v1;
 
 import com.lin.missyou.dto.TokenGetDTO;
 import com.lin.missyou.exception.http.NotFoundException;
+import com.lin.missyou.service.WxAuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,9 @@ import java.util.Map;
 @RestController
 public class TokenController {
 
+    @Autowired
+    private WxAuthenticationService wxAuthenticationService;
+
     @PostMapping("")
     public Map<String,String> getToken(@RequestBody @Validated TokenGetDTO userData){
         Map<String,String> map = new HashMap<>();
@@ -19,6 +24,7 @@ public class TokenController {
 
         switch (userData.getType()){
             case USER_WX:
+                wxAuthenticationService.code2Session(userData.getAccount());
                 break;
             case USER_Email:
                 // 这里逻辑不写了，就是去 数据库对比 account password 是否符合
--- src/main/java/com/lin/missyou/service/WxAuthenticationService.java
+++ src/main/java/com/lin/missyou/service/WxAuthenticationService.java
@@ -1,12 +1,43 @@
 package com.lin.missyou.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.MessageFormat;
+import java.util.Map;
 
 @Service
 public class WxAuthenticationService {
 
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Value("${wx.code2session}")
+    private String code2SessionUrl;
+    @Value("${wx.appid}")
+    private String appid;
+    @Value("${wx.appsecret}")
+    private String appsecret;
+
+
+
     public String code2Session(String code) {
-        // 微信进入到 小程序 会发送一个 code码  无法postman模拟
+
+        String url = MessageFormat.format(this.code2SessionUrl,this.appid,this.appsecret,code);
+
+        RestTemplate rest = new RestTemplate();
+        String sessionText = rest.getForObject(url,String.class);
+
+        try {
+            Map<String,Object> session = mapper.readValue(sessionText,Map.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
